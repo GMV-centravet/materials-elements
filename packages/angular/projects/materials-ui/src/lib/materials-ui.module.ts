3e1ff78158cc83f938cca38fc03956c68e580b40
Erwann Legeay <legeay.erwann@gmail.com>
2019-11-12 17:15:19 +0100

    Fix/ add missing materials badge declaration in ng module

--- packages/angular/projects/materials-ui/src/lib/materials-ui.module.ts
+++ packages/angular/projects/materials-ui/src/lib/materials-ui.module.ts
@@ -4,7 +4,7 @@ import { CommonModule, DOCUMENT } from '@angular/common';
 
 import { NumericValueAccessor } from '../directives/number-value-accessor';
 // tslint:disable-next-line: max-line-length
-import { MaterialsAlertController, MaterialsAutocomplete, MaterialsButton, MaterialsCard, MaterialsCardAction, MaterialsCardContent, MaterialsCheckbox, MaterialsChip, MaterialsDatatable, MaterialsDatatableAction, MaterialsDatatableBody, MaterialsDatatableBodyCell, MaterialsDatatableBodyRow, MaterialsDatatableFooter, MaterialsDatatableHeader, MaterialsDatatableHeaderColumn, MaterialsDatatablePagination, MaterialsDateField, MaterialsDatepicker, MaterialsDialog, MaterialsDialogController, MaterialsDrawer, MaterialsDrawerListItem, MaterialsDrawerWithTopAppBar, MaterialsDropdown, MaterialsExpansionPanel, MaterialsFab, MaterialsFormField, MaterialsHeadline, MaterialsIcon, MaterialsIconButton, MaterialsLinearProgress, MaterialsList, MaterialsListItem, MaterialsListItemCheckbox, MaterialsMenu, MaterialsMultipleSelect, MaterialsRadio, MaterialsRadioGroup, MaterialsSelect, MaterialsSnackbar, MaterialsSnackbarController, MaterialsStep, MaterialsStepper, MaterialsSubtitle, MaterialsSwitch, MaterialsTab, MaterialsTabs, MaterialsTextArea, MaterialsTextField, MaterialsTimeField, MaterialsTimepicker, MaterialsTopAppBar, MaterialsTopAppBarAction } from '../directives/proxies';
+import { MaterialsAlertController, MaterialsAutocomplete, MaterialsButton, MaterialsBadge, MaterialsCard, MaterialsCardAction, MaterialsCardContent, MaterialsCheckbox, MaterialsChip, MaterialsDatatable, MaterialsDatatableAction, MaterialsDatatableBody, MaterialsDatatableBodyCell, MaterialsDatatableBodyRow, MaterialsDatatableFooter, MaterialsDatatableHeader, MaterialsDatatableHeaderColumn, MaterialsDatatablePagination, MaterialsDateField, MaterialsDatepicker, MaterialsDialog, MaterialsDialogController, MaterialsDrawer, MaterialsDrawerListItem, MaterialsDrawerWithTopAppBar, MaterialsDropdown, MaterialsExpansionPanel, MaterialsFab, MaterialsFormField, MaterialsHeadline, MaterialsIcon, MaterialsIconButton, MaterialsLinearProgress, MaterialsList, MaterialsListItem, MaterialsListItemCheckbox, MaterialsMenu, MaterialsMultipleSelect, MaterialsRadio, MaterialsRadioGroup, MaterialsSelect, MaterialsSnackbar, MaterialsSnackbarController, MaterialsStep, MaterialsStepper, MaterialsSubtitle, MaterialsSwitch, MaterialsTab, MaterialsTabs, MaterialsTextArea, MaterialsTextField, MaterialsTimeField, MaterialsTimepicker, MaterialsTopAppBar, MaterialsTopAppBarAction } from '../directives/proxies';
 import { TextValueAccessor } from '../directives/text-value-accessor';
 import { SelectValueAccessor } from '../directives/select-value-accessor';
 import { RadioValueAccessor } from '../directives/radio-value-accessor';
@@ -15,6 +15,7 @@ const DECLARATIONS = [
   MaterialsAlertController,
   MaterialsAutocomplete,
   MaterialsButton,
+  MaterialsBadge,
   MaterialsCard,
   MaterialsCardAction,
   MaterialsCardContent,
