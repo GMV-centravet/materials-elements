61ca8a6cd345408642b03c2d8f39beddd7286526
Erwann Legeay <legeay.erwann@gmail.com>
2019-11-19 11:26:12 +0100

    feat/ add mutation observer to tabs

--- packages/core/src/components/tabs/tabs.tsx
+++ packages/core/src/components/tabs/tabs.tsx
@@ -16,8 +16,6 @@ export class Tabs {
 
   @Event() tabChange: EventEmitter<number>;
 
-  // @State() private tabs: HTMLMaterialsTabElement[];
-
   @Prop() color: 'background' | 'primary' | 'secondary' | 'surface' = 'background';
 
   @Prop() indicatorType: 'underline' | 'icon' = 'underline';
@@ -26,22 +24,36 @@ export class Tabs {
 
   @Prop() activeTab: number = 0;
 
-  @State() tabsContent: string[] = [];
+  @State() tabElements: HTMLMaterialsTabElement[];
 
   private mdcTabs: MDCTabBar;
 
-  async componentDidLoad() {
-    const tabs = Array.from(this.host.querySelectorAll('materials-tab'));
-    for (const tab of tabs) {
-      tab.minWidth = this.shrinkTabs;
-      tab.indicatorType = this.indicatorType;
-      await tab.renderHtml().then((content) => this.tabsContent.push(content));
-    }
-    this.tabsContent = [...this.tabsContent];
-
+  componentWillLoad() {
+    // Observe host childList mutations
+    new MutationObserver((mutationsList, _observer) => {
+      for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          this.initTabs();
+        }
+      }
+    }).observe(this.host, { attributes: false, childList: true, subtree: false });
+    this.initTabs();
   }
 
-  componentDidUpdate() {
+  initTabs() {
+    this.tabElements = Array.from(this.host.querySelectorAll('materials-tab'));
+    this.tabElements.forEach(tab => {
+      new MutationObserver((mutationsList, _observer) => {
+        for (let mutation of mutationsList) {
+          if (mutation.type === 'attributes') {
+            this.tabElements = [...this.tabElements];
+          }
+        }
+      }).observe(tab, { attributes: true, childList: false, subtree: false });
+    });
+  }
+
+  componentDidLoad() {
     this.mdcTabs = new MDCTabBar(this.host.shadowRoot.querySelector('.mdc-tab-bar'));
     this.mdcTabs.listen('MDCTabBar:activated', (tabs: CustomEvent) => {
       const selectedIndex = tabs.detail.index;
@@ -70,7 +82,7 @@ export class Tabs {
           <div class="mdc-tab-scroller">
             <div class="mdc-tab-scroller__scroll-area">
               <div class="mdc-tab-scroller__scroll-content">
-                {this.tabsContent.map(tab => tab)}
+                {this.tabElements.map(tab => tab)}
               </div>
             </div>
           </div>
