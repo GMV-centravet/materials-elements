633de4c07d0603508ce9d8702f8b70477e069558
Le Clec'h <mleclech@gmvet.fr>
2019-11-22 14:53:53 +0100

    chore(app-checkbox-demo): add demo checkbox

--- packages/site/src/app/demos/checkbox/app-checkbox-demo/app-checkbox-demo.tsx
+++ packages/site/src/app/demos/checkbox/app-checkbox-demo/app-checkbox-demo.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, Host } from '@stencil/core';
 
 
 @Component({
@@ -11,9 +11,78 @@ export class AppCheckboxDemo {
 
   render() {
     return (
-      <div>
-        <materials-checkbox></materials-checkbox>
-      </div>
+      <Host>
+        <app-demo-code-block blockTitle="Simple checkbox">
+          <materials-checkbox slot="demo" ></materials-checkbox>
+          <code slot="code">
+            {`<materials-checkbox></materials-checkbox>
+            `}
+          </code>
+        </app-demo-code-block>
+
+        <app-demo-code-block blockTitle="Checkbox with label">
+          <materials-checkbox slot="demo" alignLabel="left" label="demo" ></materials-checkbox>
+          <materials-checkbox slot="demo" alignLabel="right" label="demo" ></materials-checkbox>
+          <code slot="code">
+            {`
+             <materials-checkbox alignLabel="left" label="demo" ></materials-checkbox>
+             <materials-checkbox alignLabel="right" label="demo" ></materials-checkbox>
+            `}
+          </code>
+        </app-demo-code-block>
+
+        <app-demo-code-block blockTitle="Checkbox with color">
+          <materials-checkbox slot="demo" color="primary" checked="true" ></materials-checkbox>
+          <materials-checkbox slot="demo" checked="true" color="#1baeba"></materials-checkbox>
+          
+          <code slot="code">
+            {`
+           <materials-checkbox  color="primary" checked="true" ></materials-checkbox>
+           <materials-checkbox  checked="true" color="#1baeba"></materials-checkbox>
+            `}
+          </code>
+        </app-demo-code-block>
+
+        <app-demo-code-block blockTitle="Checkbox disabled">
+          <materials-checkbox slot="demo" disabled="true" ></materials-checkbox>
+          <materials-checkbox slot="demo" checked="true" disabled="true"></materials-checkbox>
+          
+          <code slot="code">
+            {`
+           <materials-checkbox  disabled="true" ></materials-checkbox>
+           <materials-checkbox  checked="true" disabled="true"></materials-checkbox>
+            `}
+          </code>
+        </app-demo-code-block>
+
+        <app-demo-code-block blockTitle="Checkbox check">
+          <materials-checkbox slot="demo" indeterminate="true"></materials-checkbox>
+          <materials-checkbox slot="demo" checked="indeterminate"></materials-checkbox>
+          
+          <code slot="code">
+            {`
+            <materials-checkbox indeterminate="true"></materials-checkbox>
+            <materials-checkbox checked="true"></materials-checkbox>
+            `}
+          </code>
+        </app-demo-code-block>
+
+        <app-demo-code-block blockTitle="Checkbox value">
+          <materials-checkbox slot="demo" onChange={(e) => alert(e.target.value)} label="checkbox 1" name="checkbox1" value="checkbox1"></materials-checkbox>
+          <materials-checkbox slot="demo" onChange={(e) => alert(e.target.value)} label="checkbox 2" name="checkbox2" value="checkbox2"></materials-checkbox>
+          
+          <code slot="code">
+            {`
+            <materials-checkbox onChange={(e) => alert(e.target.value)} label="checkbox 1" name="checkbox1" value="checkbox1"></materials-checkbox>
+            <materials-checkbox onChange={(e) => alert(e.target.value)} label="checkbox 2" name="checkbox2" value="checkbox2"></materials-checkbox>
+            `}
+          </code>
+        </app-demo-code-block>
+
+
+        
+
+      </Host>
     );
   }
 }
