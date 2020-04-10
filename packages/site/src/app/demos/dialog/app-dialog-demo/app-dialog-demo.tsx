a9928b8a959d4d08e8b196c9ce7b6fca5cc5669c
LeclechM <32573436+LeclechM@users.noreply.github.com>
2020-04-10 17:22:51 +0200

    fix: empty footer if no actions (#20)

--- packages/site/src/app/demos/dialog/app-dialog-demo/app-dialog-demo.tsx
+++ packages/site/src/app/demos/dialog/app-dialog-demo/app-dialog-demo.tsx
@@ -7,20 +7,26 @@ import { Component, h, Host } from '@stencil/core';
   shadow: true
 })
 export class AppDialogDemo {
-  private dialog: HTMLMaterialsDialogElement;
-
-
+  private dialogWithNoFooter: HTMLMaterialsDialogElement;
+  private dialogWithFooter: HTMLMaterialsDialogElement;
 
   render() {
     return (
       <Host>
-        <materials-button raised onClick={() => this.dialog.open()}>Open dialog</materials-button>
-        <materials-dialog ref={el => this.dialog = el} dialogTitle="Awesome dialog" acceptButton="yes" cancelButton="No">
+        <materials-button style={{ 'padding-right': '16px' }} raised onClick={() => this.dialogWithNoFooter.open()}>Open dialog no footer</materials-button>
+        <materials-button raised onClick={() => this.dialogWithFooter.open()}>Open dialog with footer</materials-button>
+        <materials-dialog ref={el => this.dialogWithNoFooter = el} dialogTitle="Awesome dialog with no footer">
+          <div slot="body">
+            Is that dialog ok to you ? see no footer ;)
+          </div>
+        </materials-dialog>
+        <materials-dialog ref={el => this.dialogWithFooter = el} acceptButton="Accepter" cancelButton="Annuler" dialogTitle="Awesome dialog">
           <div slot="body">
             Is that dialog ok to you ?
           </div>
+          <materials-button slot="third-button">Action</materials-button>
         </materials-dialog>
-      </Host>
+      </Host >
     );
   }
 }
