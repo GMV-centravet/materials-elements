508b5be820d12d1a97eda59fc36ef2274f23e440
Le Clec'h mathieu <leclech.mathieu@gmail.com>
2020-04-22 09:51:25 +0200

    feat: action accept button can be disable
    * demo with dynamically created dialog
    * same demo with toggle disable status of accept button

--- packages/site/src/app/demos/dialog/app-dialog-demo/app-dialog-demo.tsx
+++ packages/site/src/app/demos/dialog/app-dialog-demo/app-dialog-demo.tsx
@@ -1,5 +1,5 @@
 import { Component, h, Host } from '@stencil/core';
-
+import controllers from '../../../../controllers';
 
 @Component({
   tag: 'app-dialog-demo',
@@ -10,11 +10,38 @@ export class AppDialogDemo {
   private dialogWithNoFooter: HTMLMaterialsDialogElement;
   private dialogWithFooter: HTMLMaterialsDialogElement;
 
+  openDialog(): void {
+    const form = document.createElement('app-dialog-example');
+
+    controllers.dialog.create({
+      title: 'Dialog dynamically generated',
+      body: form,
+      actions: [
+        {
+          label: 'Annuler',
+          role: 'close'
+        },
+        {
+          label: 'Enregistrer',
+          role: 'accept',
+          action: () => { }
+        }
+      ]
+    }).then(async d => {
+      d.disableAcceptButton = false;
+      form.addEventListener('toggleButton', async (ev: any) => {
+        d.disableAcceptButton = !ev.detail;
+      });
+      d.open();
+    });
+  }
+
   render() {
     return (
       <Host>
         <materials-button style={{ 'padding-right': '16px' }} raised onClick={() => this.dialogWithNoFooter.open()}>Open dialog no footer</materials-button>
-        <materials-button raised onClick={() => this.dialogWithFooter.open()}>Open dialog with footer</materials-button>
+        <materials-button style={{ 'padding-right': '16px' }} raised onClick={() => this.dialogWithFooter.open()}>Open dialog with footer</materials-button>
+        <materials-button raised onClick={() => this.openDialog()}>Open dialog dynamically</materials-button>
         <materials-dialog ref={el => this.dialogWithNoFooter = el} dialogTitle="Awesome dialog with no footer">
           <div slot="body">
             Is that dialog ok to you ? see no footer ;)
